50892064d744553fe3be27dcc5577c8632012207
malikizaki94 <malikizakitop@gmail.com>
2023-10-01 23:52:47 +0700

    change to pg

--- src/config/db.go
+++ src/config/db.go
@@ -1,18 +1,18 @@
 package config
 
 import (
-	"os"
+	// "os"
 
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-	url := os.Getenv("URL")
+	// url := os.Getenv("URL")
 	var err error
-	DB, err = gorm.Open("mysql", url)
+	DB, err = gorm.Open("postgres", "host=147.139.210.135 port=5432 user=zaki dbname=zaki01 password=zaki123")
 	if err != nil {
 		panic("failed to connect database")
 	}
